8dc88f4e4b2b1f0d8222d0e5b9b3d54b05928452
Aram <arma-arman@mail.ru>
2024-11-07 14:13:49 +0400

    Changed code

--- cards.py
+++ cards.py
@@ -1,46 +1,26 @@
 import random
 
 
-def make_deck():
-    """
-        Function for making a deck:
+# def make_deck():
+#     """
+#         Function for making a deck:
 
-        We have four card suits: Clubs, Heart, Diamonds, Spades, and each of them has 13 ranks.
+#         We have four card suits: Clubs, Heart, Diamonds, Spades, and each of them has 13 ranks.
 
-        We also have a set of 13 card ranks. (2, 3, 4, ....,J, Q, K, A), we need one of the cards for each card member.
+#         We also have a set of 13 card ranks. (2, 3, 4, ....,J, Q, K, A), we need one of the cards for each card member.
 
-    """
+#     """
     
-    card_suits = set(("Clubs", "Heart", "Diamonds", "Spades"))
-    card_ranks = list(("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"))
+#     card_suits = set(("Clubs", "Heart", "Diamonds", "Spades"))
+#     card_ranks = list(("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"))
 
-    deck = list()
+#     deck = list()
 
-    for suit in card_suits:
-        for rank in card_ranks:
-            deck.append({suit: rank})
+#     for suit in card_suits:
+#         for rank in card_ranks:
+#             deck.append({suit: rank})
 
-    return deck
-
-
-def deal_deck():
-    deck = make_deck()
-    
-    new_deck = deck.copy()
-    random.shuffle(new_deck)
-
-    dealed_cards = []
-
-    for i in range(1, len(new_deck)):
-        if i <= 10:
-            dealed_cards.append(new_deck[i])
-            new_deck.remove(new_deck[i])
-        else:
-
-            break
-
-        
-    return dealed_cards
+#     return deck
 
 
 def deck_sorting(user_deck):
@@ -48,32 +28,49 @@ def deck_sorting(user_deck):
     suits_rank_numerical = []
 
     for card in user_deck:
-        for suit, rank in card.items():
-            if rank in face_ranks:
-                numeric_rank = face_ranks[rank]
-            else:
-                numeric_rank = int(rank)
+        rank = card['rank']
+        suit = card['suit']
 
-            suits_rank_numerical.append((suit, numeric_rank))
+        if rank in face_ranks:
+            numeric_rank = face_ranks[rank]
+        else:
+            numeric_rank = int(rank)
+
+        suits_rank_numerical.append({"rank": numeric_rank, "suit": suit})
 
 
-        suits_rank_numerical.sort(key=lambda x: (x[0], x[1]))
+    suits_rank_numerical.sort(key=lambda x: (x['suit'], x['rank']))
     
     def to_letters(deck):
         converted_deck = []
         reverse_face_ranks = {v: k for k, v in face_ranks.items()}
-        print(deck)
 
-        for suit, rank in deck:
-            print("suit ", suit, "rank", rank)
-            rank = reverse_face_ranks.get(rank, str(rank)) 
-            converted_deck.append((suit, rank)) 
+        for card in deck:
+            rank = card['rank']
+            suit = card['suit']
+
+            if rank in reverse_face_ranks:
+                rank = reverse_face_ranks[rank]
+
+            converted_deck.append({"rank": rank, "suit": suit}) 
+
+
         return converted_deck
 
     return to_letters(suits_rank_numerical)
 
 
-user_deck = deal_deck()
+user_deck = [
+    { "rank": 'J', "suit": 'Clubs'},
+    { "rank": 'J', "suit": 'Heart'},
+    { "rank": '7', "suit": 'Diamonds'},
+    { "rank": '8', "suit": 'Diamonds'},
+    { "rank": '9', "suit": 'Spades'},
+    { "rank": '2', "suit": 'Clubs'},
+    { "rank": '3', "suit": 'Heart'},
+    { "rank": '5', "suit": 'Diamonds'},
+    { "rank": '10', "suit": 'Clubs'},
+    { "rank": 'J', "suit": 'Diamonds'}]
 
-# print(user_deck, "\n")
+
 print(deck_sorting(user_deck))
